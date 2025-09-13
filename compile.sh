447051566a91c48a0996fa27cb2bf3ef5aacdf2a
xdeadstokex <129391255+xdeadstokex@users.noreply.github.com>
2025-09-13 21:04:56 +0700

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $script_dir
 
+./libs/compile.sh
+
 gcc -c roman_number_converter.c -o roman_number_converter.o -I./libs -I./
 gcc -o main core_base.c roman_number_converter.o ./libs/texts.o -I./libs -I./
